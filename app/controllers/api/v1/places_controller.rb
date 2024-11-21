d3564ac11b98e2f5657cd2a31ae30b071e364bc8
amir-ali <amirtoali1@gmail.com>
2024-11-21 16:24:30 +0500

    in this i fix the errors

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -1,12 +1,36 @@
 class Api::V1::PlacesController < ApplicationController
+	before_action :set_place, only: [:show,  :update, :destroy]
 	def index 
 		@places = Place.all
-		byebug
 		render json: @places
 	end
 	def show 
-		@place = Place.find(params[:id])
-		render json: @place
+	 render json: @place
 	end
+	def update
+	 if @place.update(place_params)
+      render json: @place
+	 end
+	end
+	def create
+	 @place = Place.create(place_params)
+		if @place.save
+		  render json: @place
+		 end
+	end
+	def destroy
+	 if @place.destroy
+	 	render json: {status: "success", messages:"destroy is successfully"}
+	 end
+	end
+	private
+	def set_place
+		@place = Place.find(params[:id])
+      rescue ActiveRecord::RecordNotFound => errors
+       render json: {status: "fail", messages:errors.message}
+  end
 
+	def place_params
+		params.require(:place).permit(:name, :description, :image_url, :langitude, :latitude, :state, :city, :country)
+	end
 end
